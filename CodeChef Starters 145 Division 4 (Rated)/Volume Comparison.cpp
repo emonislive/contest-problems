a828c8df0e09b9c0c0db8be1d02081aca2d9f65d
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-31 22:45:19 +0600

    not happy with the result :(

---
+++ CodeChef Starters 145 Division 4 (Rated)/Volume Comparison.cpp
@@ -0,0 +1,24 @@
+// problem code: CABLE
+#include <iostream>
+using namespace std;
+#define ll long long
+#define endl '\n'
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int vol = a * b * c;
+    int vol_cube = d * d * d;
+    
+    if (vol == vol_cube)
+        cout << "Equal" << endl;
+    else if (vol > vol_cube)
+        cout << "Cuboid" << endl;
+    else
+        cout << "Cube" << endl;
+
+    return 0;
+}
