733a12b29afce949847ebf4be9da5757f967ff11
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-08-07 00:37:45 +0600

    Problem A (comment added)

--- Codeforces Round 964 (Div. 4)/A - A+B Again?.cpp
+++ Codeforces Round 964 (Div. 4)/A - A+B Again?.cpp
@@ -1,12 +1,12 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>      // It is basically a header file that includes every standard library
 using namespace std;
 #define endl '\n'
 #define ll long long
 
 void solve(int n){
-    int num1 = n % 10;
-    int num2 = n / 10;
-    int total = num1 + num2;
+    int num1 = n % 10;        // gets the last num, ex. num = 21,     21 % 10 = 1 
+    int num2 = n / 10;        // gets the 2nd last num                21 / 10 = 2
+    int total = num1 + num2; 
     cout << total << endl;
 }
 
