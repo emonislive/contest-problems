e6b1a46442830ecace53b59d4093cce26b1bfeee
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-08-10 13:07:28 +0600

    219158B

---
+++ Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/B. Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    int a; long long int b; char c; float d; double e;
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a << endl << b << endl;
+    cout << c << endl << d << endl;
+    cout << e;
+
+    return 0;
+}
