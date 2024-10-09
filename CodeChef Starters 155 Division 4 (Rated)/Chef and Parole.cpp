cd236b9307828dd2ec49b15f4542235c35644d58
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-10-09 21:16:09 +0600

    CHEFPAROLE

---
+++ CodeChef Starters 155 Division 4 (Rated)/Chef and Parole.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+void solve() {
+    int n;
+    cin >> n;
+    if (n >= 7)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
