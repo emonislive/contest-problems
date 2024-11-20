31e6022da6fd02d973f9da71c45f0e736c63391c
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-11-20 21:48:08 +0600

    NUGGET

---
+++ CodeChef Starters 161 Division 4 (Rated)/Moneymaking.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+void solve(int a, int b)
+{
+    a = a * 5000;
+    b = b * 9800;
+    cout << a + b << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int a, b;
+  
+    cin >> a >> b;
+    solve(a, b);
+  
+    return 0;
+}
